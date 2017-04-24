d087229062261dc7ef97bf96432eaba4e928dab8
Andrew Sharp <andrew.sharp57@gmail.com>
2017-04-24 18:39:16 -0500

    use bash to set npm version

--- ci.bash
+++ ci.bash
@@ -1,6 +1,7 @@
 # written for Travis CI
 
 set -xeuo pipefail
+if [[ $(npm -v) != 4* ]]; then npm i -g npm@4; fi
 npm --version  # TODO: remove this line
 ./gradlew assemble check
 if [ "${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" = 'master' ]; then
