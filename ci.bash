3e0b1ea7e3a08e472ae539e9307ca41a8acdd8b1
Andrew Sharp <andrew.sharp57@gmail.com>
2017-04-24 18:12:38 -0500

    debug deployment

--- ci.bash
+++ ci.bash
@@ -1,4 +1,8 @@
+set -xeuo pipefail
 ./gradlew assemble check
+git branch
+git branch | grep '*'
+git branch | grep '*' | cut -d' ' -f2
 if [ "$(git branch | grep '*' | cut -d' ' -f2)" = 'master' ]; then
   npm install -g serverless
   serverless deploy
