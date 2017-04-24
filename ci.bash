309ee73d331af4e351ddd72a7932d7d9364f4903
Andrew Sharp <andrew.sharp57@gmail.com>
2017-04-24 18:41:16 -0500

    move version set to if

--- ci.bash
+++ ci.bash
@@ -1,10 +1,9 @@
 # written for Travis CI
 
 set -xeuo pipefail
-if [[ $(npm -v) != 4* ]]; then npm i -g npm@4; fi
-npm --version  # TODO: remove this line
 ./gradlew assemble check
 if [ "${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" = 'master' ]; then
+  if [[ $(npm -v) != 4* ]]; then npm i -g npm@4; fi
   npm install -g serverless
   serverless deploy --stage dev --region us-east-1 --verbose
 fi
