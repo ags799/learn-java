d583a9df1a027e64485a51b02868d77ec5cf1a1e
Andrew Sharp <andrew.sharp57@gmail.com>
2017-04-24 20:11:26 -0500

    Handler checkstyle

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -1,25 +1,24 @@
 package com.serverless;
 
-import java.util.Collections;
-import java.util.Map;
-
-import org.apache.log4j.Logger;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import java.util.Collections;
+import java.util.Map;
+import org.apache.log4j.Logger;
 
 public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
-	private static final Logger LOG = Logger.getLogger(Handler.class);
+  private static final Logger LOG = Logger.getLogger(Handler.class);
 
-	@Override
+  @Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-		LOG.info("received: " + input);
-		Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
-		return ApiGatewayResponse.builder()
-				.setStatusCode(200)
-				.setObjectBody(responseBody)
-				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
-				.build();
-	}
+    LOG.info("received: " + input);
+    Response responseBody = new Response(
+        "Go Serverless v1.x! Your function executed successfully!", input);
+    return ApiGatewayResponse.builder()
+        .setStatusCode(200)
+        .setObjectBody(responseBody)
+        .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+        .build();
+  }
 }
