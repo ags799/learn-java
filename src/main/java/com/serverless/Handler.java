d7eb2e3888b6b7fe78c400413b036a7d9cf3a4f9
Andrew Sharp <ags799@users.noreply.github.com>
2017-04-25 18:59:27 -0500

    use jrestless (#43)
    * use jrestless
    
    * checkstyle & findbugs

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -1,31 +1,16 @@
 package com.serverless;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.ImmutableMap;
-import java.util.Map;
-import org.apache.log4j.Logger;
+import com.jrestless.aws.gateway.GatewayFeature;
+import com.jrestless.aws.gateway.handler.GatewayRequestObjectHandler;
+import org.glassfish.jersey.server.ResourceConfig;
 
-public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
-
-  private static final Logger LOG = Logger.getLogger(Handler.class);
-
-  @Override
-  public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-    LOG.info("received: " + input);
-    Response response = ImmutableResponse.builder().message("some message").build();
-    ObjectMapper objectMapper = new ObjectMapper();
-    try {
-      return ImmutableApiGatewayResponse.builder()
-          .statusCode(200)
-          .headers(ImmutableMap.of("X-Powered-By", "AWS Lambda & serverless"))
-          .body(objectMapper.writeValueAsString(response))
-          .isBase64Encoded(false)
-          .build();
-    } catch (JsonProcessingException e) {
-      throw new RuntimeException(e);
-    }
+public class Handler extends GatewayRequestObjectHandler {
+  /** copied from https://github.com/bbilger/jrestless#aws-usage-example */
+  public Handler() {
+    ResourceConfig resourceConfig = new ResourceConfig()
+        .register(GatewayFeature.class)
+        .packages("com.serverless");
+    init(resourceConfig);
+    start();
   }
 }
