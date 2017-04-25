987bff2b61cc945dd312447e11eeff1d65bacc12
Andrew Sharp <andrew.sharp57@gmail.com>
2017-04-25 12:57:48 -0500

    immutable ApiGatewayResponse

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -2,7 +2,9 @@ package com.serverless;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import java.util.Collections;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableMap;
 import java.util.Map;
 import org.apache.log4j.Logger;
 
@@ -13,10 +15,17 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
   @Override
   public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
     LOG.info("received: " + input);
-    return ApiGatewayResponse.builder()
-        .setStatusCode(200)
-        .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
-        .setObjectBody(ImmutableResponse.builder().message("some message").build())
-        .build();
+    Response response = ImmutableResponse.builder().message("some message").build();
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+      return ImmutableApiGatewayResponse.builder()
+          .statusCode(200)
+          .headers(ImmutableMap.of("X-Powered-By", "AWS Lambda & serverless"))
+          .body(objectMapper.writeValueAsString(response))
+          .isBase64Encoded(false)
+          .build();
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
