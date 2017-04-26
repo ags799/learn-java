1fa2df3ed817447620b59a2f2e75df877c98a2b0
Andrew Sharp <andrew.sharp57@gmail.com>
2017-04-26 07:38:03 -0500

    remove logging

--- src/main/java/com/serverless/GraphResource.java
+++ src/main/java/com/serverless/GraphResource.java
@@ -17,13 +17,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Path("/api/graph")
 public class GraphResource {
-  private static final Logger log = LoggerFactory.getLogger(GraphResource.class);
-
   private final EdgeDao edgeDao = DynamoDbEdgeDao.getInstance();
 
   /** Returns the graph for given ID. */
@@ -31,7 +27,6 @@ public class GraphResource {
   @Path("/{id}")
   @Produces(MediaType.APPLICATION_JSON)
   public Graph get(@PathParam("id") String id) {
-    log.error("get-by-id");
     Set<StorageEdge> storageEdges = edgeDao.get(id);
     if (storageEdges.isEmpty()) {
       throw new NotFoundException();
@@ -51,7 +46,6 @@ public class GraphResource {
   @Path("/")
   @Produces(MediaType.APPLICATION_JSON)
   public Set<Graph> getAll() {
-    log.error("get-all");
     Set<StorageEdge> storageEdges = edgeDao.getAll();
     return storageEdges.stream()
         .collect(Collectors.groupingBy(StorageEdge::getGraphId, Collectors.toSet()))
@@ -73,7 +67,6 @@ public class GraphResource {
   @PUT
   @Path("/{id}")
   public void put(@PathParam("id") String id, Graph graph) {
-    log.error("put");
     Set<StorageEdge> storageEdges = graph.edges().stream()
         .map(edge -> {
           StorageEdge storageEdge = new StorageEdge();
@@ -90,7 +83,6 @@ public class GraphResource {
   @DELETE
   @Path("/{id}")
   public void delete(@PathParam("id") String id) {
-    log.error("delete");
     edgeDao.delete(id);
   }
 }
--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -1,17 +1,11 @@
 package com.serverless;
 
 import com.jrestless.aws.gateway.GatewayFeature;
-import com.jrestless.aws.gateway.handler.GatewayRequestAndLambdaContext;
 import com.jrestless.aws.gateway.handler.GatewayRequestObjectHandler;
-import com.jrestless.core.container.io.JRestlessContainerRequest;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
 public class Handler extends GatewayRequestObjectHandler {
-  private static final Logger log = LoggerFactory.getLogger(Handler.class);
-
   /** copied from https://github.com/bbilger/jrestless#aws-usage-example */
   public Handler() {
     SLF4JBridgeHandler.removeHandlersForRootLogger();
@@ -21,10 +15,5 @@ public class Handler extends GatewayRequestObjectHandler {
         .packages("com.serverless");
     init(resourceConfig);
     start();
-  }
-
-  @Override
-  protected void beforeHandleRequest(GatewayRequestAndLambdaContext request, JRestlessContainerRequest containerRequest) {
-    log.error("beforeHandleRequest");
   }
 }
