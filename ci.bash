a5f503f96858c17b741918315c2f9d893527048e
Andrew Sharp <andrew.sharp57@gmail.com>
2017-04-26 08:11:22 -0500

    fix

--- ci.bash
+++ ci.bash
@@ -1,14 +1,16 @@
 # written for Travis CI
 
 set -xeuo pipefail
-./gradlew assemble check
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+# https://github.com/creationix/nvm/issues/1505
+sed -i '2474,2476s/$REINSTALL_PACKAGES_FROM/${REINSTALL_PACKAGES_FROM:-}/' /home/username/.nvm/nvm.sh
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install 4
 nvm use 4
 node --version
 npm -v
+./gradlew assemble check
 if [ "${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" = 'master' ]; then
   npm install -g serverless
   serverless deploy --stage dev --region us-east-1 --verbose
