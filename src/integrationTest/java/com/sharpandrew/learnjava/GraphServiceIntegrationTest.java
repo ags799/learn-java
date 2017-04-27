4c65924329da1f89e9bc9537734a39b4932cbfe3
Andrew Sharp <andrew.sharp57@gmail.com>
2017-04-27 17:26:31 -0500

    use feign

--- src/integrationTest/java/com/sharpandrew/learnjava/GraphServiceIntegrationTest.java
+++ src/integrationTest/java/com/sharpandrew/learnjava/GraphServiceIntegrationTest.java
@@ -2,27 +2,29 @@ package com.sharpandrew.learnjava;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import org.apache.commons.io.IOUtils;
+import feign.Feign;
+import feign.jackson.JacksonDecoder;
+import feign.jaxrs.JAXRSContract;
 import org.junit.Before;
 import org.junit.Test;
 
 public class GraphServiceIntegrationTest {
-  private String url;
+  private GraphService graphService;
 
   /** Checkstyle wants javadoc. */
   @Before
   public void setUp() throws Exception {
     String dirtyUrl = System.getenv("LEARN_JAVA_URL");
-    assert dirtyUrl.endsWith("{proxy+}");
-    url = dirtyUrl.substring(0, dirtyUrl.length() - "{proxy+}".length());
-    System.out.println("cleaned url: " + url);
+    assert dirtyUrl.endsWith("api/{proxy+}");
+    String url = dirtyUrl.substring(0, dirtyUrl.length() - "api/{proxy+}".length());
+    graphService = Feign.builder()
+        .decoder(new JacksonDecoder())
+      .contract(new JAXRSContract())
+      .target(GraphService.class, url);
   }
 
   @Test
   public void initiallyThereAreNoGraphs() throws Exception {
-    assertThat(IOUtils.toString(new URL(url + "graph").openStream(), StandardCharsets.UTF_8))
-        .isEqualTo("[]");
+    assertThat(graphService.getAll()).isEmpty();
   }
 }
