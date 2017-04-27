a8aa317bbdd5797d5a60092029045fe39898f435
Andrew Sharp <andrew.sharp57@gmail.com>
2017-04-27 16:34:09 -0500

    ci runs integration tests

--- ci.bash
+++ ci.bash
@@ -1,8 +1,18 @@
 # written for Travis CI
 
 set -xeuo pipefail
+
 ./gradlew assemble check
-if [ "${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" = 'master' ]; then
+
+# integration tests
+branch="${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}"
+stage="ci-$branch"
+./gradlew deploy "-Pstage=$stage"
+url="$(serverless info --stage "$stage" | grep -C 1 '^endpoints:' | sed -n 3p | cut -d' ' -f5
+LEARN_JAVA_URL="$url" ./gradlew integrationTest
+
+# continuous deployment
+if [[ "$branch" = 'master' ]]; then
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
   # https://github.com/creationix/nvm/issues/1505
   sed -i '2474,2476s/$REINSTALL_PACKAGES_FROM/${REINSTALL_PACKAGES_FROM:-}/' /home/travis/.nvm/nvm.sh
