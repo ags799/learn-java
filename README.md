d51c18dddad244b8694afc3dc169ddd6714e4923
Andrew Sharp <andrew.sharp57@gmail.com>
2017-04-27 16:28:54 -0500

    integration test

--- README.md
+++ README.md
@@ -19,3 +19,16 @@ Deploy your code with
     ./gradlew deploy
 
 The output of the deploy command will indicate the URL to which the code was deployed.
+
+To run integration tests, first deploy your code to some development stack
+
+    ./gradlew deploy -Pstage=my-dev-stack
+
+This command will print a URL for the new stack. Run the tests with the URL for that stack in the
+`LEARN_JAVA_URL` environment variable.
+
+    LEARN_JAVA_URL=http://something.com/api ./gradlew integrationTest
+
+Remove your deployment with
+
+    ./gradlew removeDeployment -Pstage=my-dev-stack
---
+++ src/integrationTest/java/com/sharpandrew/learnjava/GraphServiceIntegrationTest.java
@@ -0,0 +1,26 @@
+package com.sharpandrew.learnjava;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.io.IOUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GraphServiceIntegrationTest {
+  private String url;
+
+  @Before
+  public void setUp() throws Exception {
+    String dirtyUrl = System.getenv("LEARN_JAVA_URL");
+    assert dirtyUrl.endsWith("{proxy+}");
+    url = dirtyUrl.substring(0, dirtyUrl.length() - "{proxy+}".length());
+    System.out.println("cleaned url: " + url);
+  }
+
+  @Test
+  public void initiallyThereAreNoGraphs() throws Exception {
+    assertThat(IOUtils.toString(new URL(url + "graph").openStream(), StandardCharsets.UTF_8)).isEqualTo("[]");
+  }
+}
