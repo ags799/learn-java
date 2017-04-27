ba48103b619099f1a56210848d8c8dda63a580ca
Andrew Sharp <andrew.sharp57@gmail.com>
2017-04-27 16:53:27 -0500

    always remove deployment

--- ci.bash
+++ ci.bash
@@ -2,21 +2,13 @@
 
 set -xeuo pipefail
 
-./gradlew assemble check
-
-# integration tests
-installServerless
-branch="${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}"
-stage="ci-$branch"
-./gradlew deploy "-Pstage=$stage"
-url="$(serverless info --stage "$stage" | grep -C 1 '^endpoints:' | sed -n 3p | cut -d' ' -f5)"
-LEARN_JAVA_URL="$url" ./gradlew integrationTest
-./gradlew removeDeployment "-Pstage=$stage"
-
-# continuous deployment
-if [[ "$branch" = 'master' ]]; then
-  ./gradlew deploy -Pstage=prod
-fi
+installNvm() {
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+  # https://github.com/creationix/nvm/issues/1505
+  sed -i '2474,2476s/$REINSTALL_PACKAGES_FROM/${REINSTALL_PACKAGES_FROM:-}/' /home/travis/.nvm/nvm.sh
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+}
 
 installServerless() {
   installNvm
@@ -25,10 +17,20 @@ installServerless() {
   npm install -g serverless
 }
 
-installNvm() {
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
-  # https://github.com/creationix/nvm/issues/1505
-  sed -i '2474,2476s/$REINSTALL_PACKAGES_FROM/${REINSTALL_PACKAGES_FROM:-}/' /home/travis/.nvm/nvm.sh
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-}
+./gradlew assemble check
+
+# integration tests
+installServerless
+branch="${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}"
+stage="ci-$branch"
+./gradlew deploy "-Pstage=$stage"
+url="$(serverless info --stage "$stage" | grep -C 1 '^endpoints:' | sed -n 3p | cut -d' ' -f5)"
+LEARN_JAVA_URL="$url" ./gradlew integrationTest ||
+    (./gradlew removeDeployment "-Pstage=$stage" && exit 1)
+./gradlew removeDeployment "-Pstage=$stage"
+
+# continuous deployment
+if [[ "$branch" = 'master' ]]; then
+  ./gradlew deploy -Pstage=prod
+fi
+
