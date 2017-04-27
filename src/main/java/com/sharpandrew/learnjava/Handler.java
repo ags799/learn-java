5c52ea3ab5b7abbe0f32f300c3dbe44eb0767648
Andrew Sharp <andrew.sharp57@gmail.com>
2017-04-26 19:55:54 -0500

    WIP swagger

--- src/main/java/com/sharpandrew/learnjava/Handler.java
+++ src/main/java/com/sharpandrew/learnjava/Handler.java
@@ -2,6 +2,9 @@ package com.sharpandrew.learnjava;
 
 import com.jrestless.aws.gateway.GatewayFeature;
 import com.jrestless.aws.gateway.handler.GatewayRequestObjectHandler;
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
@@ -12,7 +15,12 @@ public final class Handler extends GatewayRequestObjectHandler {
     SLF4JBridgeHandler.install();
     ResourceConfig resourceConfig = new ResourceConfig()
         .registerInstances(GraphResource.create())
-        .register(GatewayFeature.class);
+        .register(GatewayFeature.class)
+        // these classes are for generating Swagger definitions
+        .registerClasses(ApiListingResource.class, SwaggerSerializers.class);
+    BeanConfig beanConfig = new BeanConfig();
+    beanConfig.setBasePath("/api");
+    beanConfig.setScan(true);
     init(resourceConfig);
     start();
   }
