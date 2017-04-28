1e19b6fc74197480dceb18d9ae03ca5572a48059
Andrew Sharp <andrew.sharp57@gmail.com>
2017-04-27 23:04:20 -0500

    turn on SLS_DEBUG

--- ci.bash
+++ ci.bash
@@ -24,8 +24,7 @@ installServerless
 branch="${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}"
 stage="ci-${branch//\//-}"
 mkdir -p src/integrationTest/resources
-serverless package --stage "$stage"
-serverless deploy --stage "$stage" --verbose > src/integrationTest/resources/most-recent-deployment-output.txt
+SLS_DEBUG=true serverless deploy --stage "$stage" --verbose > src/integrationTest/resources/most-recent-deployment-output.txt
 ./gradlew integrationTest || (serverless remove --stage "$stage" && exit 1)
 serverless remove --stage "$stage"
 
