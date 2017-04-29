7ed10945f051441585055c1a8c5de9fd320f345e
Andrew Sharp <ags799@users.noreply.github.com>
2017-04-29 13:53:45 -0500

    remove DaoFactory & checkstyle for Javadoc (#80)

--- src/main/java/com/sharpandrew/learnjava/GraphService.java
+++ src/main/java/com/sharpandrew/learnjava/GraphService.java
@@ -12,24 +12,20 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/api/graph")
 public interface GraphService {
-  /** Returns the graph for given ID. */
   @GET
   @Path("/{id}")
   @Produces(MediaType.APPLICATION_JSON)
   Graph get(@PathParam("id") String id);
 
-  /** Returns all graphs. */
   @GET
   @Path("/")
   @Produces(MediaType.APPLICATION_JSON)
   Set<Graph> getAll();
 
-  /** Adds a graph. */
   @PUT
   @Path("/{id}")
   void put(@PathParam("id") String id, Graph graph);
 
-  /** Deletes the graph for given ID. */
   @DELETE
   @Path("/{id}")
   void delete(@PathParam("id") String id);
--- src/main/java/com/sharpandrew/learnjava/ServerlessEnvironment.java
+++ src/main/java/com/sharpandrew/learnjava/ServerlessEnvironment.java
@@ -1,6 +1,6 @@
 package com.sharpandrew.learnjava;
 
-/** Values here must match serverless.yml. */
+// Values here must match serverless.yml.
 public final class ServerlessEnvironment {
   public static String getEdgesTableName() {
     return System.getenv("EDGES_TABLE_NAME");
