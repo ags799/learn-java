e264927fa446e22017a055dbe005bcf1b4510288
Andrew Sharp <andrew.sharp57@gmail.com>
2017-04-29 14:37:40 -0500

    extract password hasher

--- src/main/java/com/sharpandrew/learnjava/UserResource.java
+++ src/main/java/com/sharpandrew/learnjava/UserResource.java
@@ -5,49 +5,26 @@ import static com.google.common.base.Preconditions.checkArgument;
 import com.google.common.annotations.VisibleForTesting;
 import com.sharpandrew.learnjava.storage.DynamoDbUserDao;
 import com.sharpandrew.learnjava.storage.UserDao;
-import java.nio.charset.StandardCharsets;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
 
 public final class UserResource implements UserService {
+  private final PasswordHasher passwordHasher;
   private final UserDao userDao;
 
   @VisibleForTesting
-  UserResource(UserDao userDao) {
+  UserResource(PasswordHasher passwordHasher, UserDao userDao) {
+    this.passwordHasher = passwordHasher;
     this.userDao = userDao;
   }
 
   public static UserResource create() {
-    return new UserResource(DynamoDbUserDao.getInstance());
+    return new UserResource(new DefaultPasswordHasher(), DynamoDbUserDao.getInstance());
   }
 
   @Override
   public void put(String email, String password) {
     checkArgument(!userDao.containsEmail(email), "A user with email '%s' already exists.", email);
-    String salt = new String(new SecureRandom().generateSeed(32), StandardCharsets.UTF_8);
-    String passwordHash = getPasswordHash(password, salt);
+    String salt = passwordHasher.createSalt();
+    String passwordHash = passwordHasher.getPasswordHash(password, salt);
     userDao.put(email, passwordHash, salt);
-  }
-
-  // http://howtodoinjava.com/security/how-to-generate-secure-password-hash-md5-sha-pbkdf2-bcrypt-examples/
-  private static String getPasswordHash(String password, String salt) {
-    SecretKeyFactory factory;
-    try {
-      factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-    } catch (NoSuchAlgorithmException e) {
-      throw new RuntimeException(e);
-    }
-    PBEKeySpec keySpec = new PBEKeySpec(
-        password.toCharArray(), salt.getBytes(StandardCharsets.UTF_8), 1000, 64 * 8);
-    byte[] hash;
-    try {
-      hash = factory.generateSecret(keySpec).getEncoded();
-    } catch (InvalidKeySpecException e) {
-      throw new RuntimeException(e);
-    }
-    return new String(hash, StandardCharsets.UTF_8);
   }
 }
