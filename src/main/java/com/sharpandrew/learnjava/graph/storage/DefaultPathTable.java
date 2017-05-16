a281ccc642380c879869783c31f4692eec1a3861
Andrew Sharp <andrew.sharp57@gmail.com>
2017-05-16 10:20:17 -0500

    DefaultPathTable uses DynamoDbDao

--- src/main/java/com/sharpandrew/learnjava/graph/storage/DefaultPathTable.java
+++ src/main/java/com/sharpandrew/learnjava/graph/storage/DefaultPathTable.java
@@ -1,16 +1,10 @@
 package com.sharpandrew.learnjava.graph.storage;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
-import com.amazonaws.services.dynamodbv2.model.Condition;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.Iterables;
 import com.sharpandrew.learnjava.graph.model.GraphPath;
 import com.sharpandrew.learnjava.graph.model.ImmutableGraphPath;
+import com.sharpandrew.learnjava.graph.storage.dynamodb.DynamoDbDao;
 import com.sharpandrew.learnjava.graph.storage.dynamodb.DynamoDbPathEdge;
 import com.sharpandrew.learnjava.serverless.Environment;
 import java.util.List;
@@ -22,38 +16,25 @@ import java.util.stream.IntStream;
 public class DefaultPathTable implements PathTable {
   private static DefaultPathTable instance = null;
 
-  private final DynamoDBMapper mapper;
+  private final Dao<DynamoDbPathEdge> dao;
 
   @VisibleForTesting
-  DefaultPathTable(DynamoDBMapper mapper) {
-    this.mapper = mapper;
+  DefaultPathTable(Dao<DynamoDbPathEdge> dao) {
+    this.dao = dao;
   }
 
   public static DefaultPathTable getInstance() {
     if (instance == null) {
-      String tableName = Environment.getPathTableName();
-      DynamoDBMapperConfig config = DynamoDBMapperConfig.builder()
-          .withTableNameOverride(
-              DynamoDBMapperConfig.TableNameOverride.withTableNameReplacement(tableName))
-          .build();
-      DynamoDBMapper mapper = new DynamoDBMapper(
-          AmazonDynamoDBClientBuilder.defaultClient(), config);
-      instance = new DefaultPathTable(mapper);
-      return instance;
-    } else {
-      return instance;
+      Dao<DynamoDbPathEdge> dao =
+          DynamoDbDao.create(DynamoDbPathEdge.class, Environment.getPathTableName());
+      instance = new DefaultPathTable(dao);
     }
+    return instance;
   }
 
   @Override
   public GraphPath get(String pathId) {
-    DynamoDBScanExpression scanExpression = new DynamoDBScanExpression();
-    scanExpression.addFilterCondition(
-        "pathId",
-        new Condition()
-            .withComparisonOperator(ComparisonOperator.EQ)
-            .withAttributeValueList(new AttributeValue(pathId)));
-    List<DynamoDbPathEdge> matchingEdges = mapper.scan(DynamoDbPathEdge.class, scanExpression);
+    List<DynamoDbPathEdge> matchingEdges = dao.getMatchingRows("pathId", pathId);
     List<Integer> vertices = matchingEdges.stream()
         .map(DynamoDbPathEdge::getStartVertex)
         .collect(Collectors.toList());
@@ -78,19 +59,7 @@ public class DefaultPathTable implements PathTable {
           return dynamoDbPathEdge;
         })
         .collect(Collectors.toSet());
-    List<DynamoDBMapper.FailedBatch> failedBatches = mapper.batchSave(dynamoDbPathEdges);
-    if (!failedBatches.isEmpty()) {
-      StringBuilder stringBuilder = new StringBuilder();
-      stringBuilder.append("Failed to save some batches:\n");
-      failedBatches.forEach(batch -> {
-        stringBuilder.append('\t');
-        stringBuilder.append(batch.getUnprocessedItems());
-        stringBuilder.append("\t\t");
-        stringBuilder.append(batch.getException());
-      });
-      throw new RuntimeException(stringBuilder.toString());
-    } else {
-      return pathId;
-    }
+    dao.saveAll(dynamoDbPathEdges);
+    return pathId;
   }
 }
