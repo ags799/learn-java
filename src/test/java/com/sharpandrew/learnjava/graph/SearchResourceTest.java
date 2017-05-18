6313e232219ca1e0df4729fde40595e5c1b2476a
Andrew Sharp <andrew.sharp57@gmail.com>
2017-05-18 12:12:29 -0500

    test

--- src/test/java/com/sharpandrew/learnjava/graph/SearchResourceTest.java
+++ src/test/java/com/sharpandrew/learnjava/graph/SearchResourceTest.java
@@ -65,4 +65,63 @@ public final class SearchResourceTest {
     assertThat(searchResource.breadthFirstSearch("some-id", 0)).containsExactly(
         Vertex.create(0), Vertex.create(1), Vertex.create(2), Vertex.create(3), Vertex.create(4));
   }
+
+  /**
+   * 0
+   * |
+   * 1
+   *
+   * should return [0, 1]
+   */
+  @Test
+  public void depthFirstSearch_oneEdge() throws Exception {
+    Graph graph = Graph.create("some-id", 0, 1);
+    when(graphTable.get("some-id")).thenReturn(graph);
+    assertThat(searchResource.depthFirstSearch("some-id", 0))
+        .containsExactly(Vertex.create(0), Vertex.create(1));
+  }
+
+  /**
+   *   0
+   *  / \
+   * 1  2
+   *
+   * should return [0, 1, 2]
+   */
+  @Test
+  public void depthFirstSearch_threeNodeBinaryTree() throws Exception {
+    Graph graph = Graph.create(
+        "some-id",
+        0, 1,
+        0, 2);
+    when(graphTable.get("some-id")).thenReturn(graph);
+    assertThat(searchResource.depthFirstSearch("some-id", 0))
+        .containsExactly(Vertex.create(0), Vertex.create(1), Vertex.create(2));
+  }
+
+  /**
+   *     0
+   *    / \
+   *   1  4
+   *  / \
+   * 2  3
+   *
+   * should return [0, 1, 2, 3, 4]
+   */
+  @Test
+  public void depthFirstSearch_fiveNodeBinaryTree() throws Exception {
+    Graph graph = Graph.create(
+        "some-id",
+        0, 1,
+        0, 4,
+        1, 2,
+        1, 3);
+    when(graphTable.get("some-id")).thenReturn(graph);
+    assertThat(searchResource.depthFirstSearch("some-id", 0)).containsExactly(
+        Vertex.create(0),
+        Vertex.create(1),
+        Vertex.create(2),
+        Vertex.create(3),
+        Vertex.create(4));
+  }
 }
